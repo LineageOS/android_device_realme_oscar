8a5fb7222d89579475e3bc686d910357b4eba777
Vivekachooz <vivekachooz@gmail.com>
2023-09-29 20:07:00 +0530

    oscar: Commonize TARGET_KERNEL_CONFIG
    Change-Id: Idf4f32bb7d5dce7b31e0d929ea8aa520b94f401f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,5 @@
 
 DEVICE_PATH := device/realme/oscar
 
-# Kernel
-TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig
-
 # Inherit the proprietary files
 -include vendor/realme/oscar/BoardConfigVendor.mk
