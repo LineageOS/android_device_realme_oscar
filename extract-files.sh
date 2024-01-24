adae0615cc101c726403a30f015a83ece05afa4c
Vivekachooz <vivekachooz@gmail.com>
2024-01-24 15:20:52 +0530

    oscar: Decommonize ultrawide camera blob fixup
    Change-Id: I16e90bd9b1fd7eb3960b9e691dad1b3f0f6905e8

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/overlayfs/in/lib64/hw/com.qti.chi.override.so)
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
